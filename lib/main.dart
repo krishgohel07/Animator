02ea2195e63827b2c6a92da992d3fd6d27b997cd
krishgohel07 <krishgohel6289@gmail.com>
2023-12-31 23:31:07 +0530

    Animator

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(""),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/image/1.jpg"))),
+        ),
+      ),
+    );
+  }
+}
